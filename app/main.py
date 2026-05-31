7ec16070a8a78701d63930af9ba4accd983884fe
Piyush Ramteke <piyu.143247@gmail.com>
2026-05-31 13:17:17 +0530

    feat: initialize FastAPI application with route configuration and CORS middleware

--- app/main.py
+++ app/main.py
@@ -16,13 +16,8 @@ app = FastAPI(title="Piyu API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:8501",
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:8501",
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
